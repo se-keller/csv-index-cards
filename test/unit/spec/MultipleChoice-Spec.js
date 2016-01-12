ac7e2bec42a3f7f23f5eee68e5ec6d94b8abb764
se-keller <sebastian.sekell@googlemail.com>
2016-01-12 01:16:25 +0100

    Refactored MultipleChoice Test

--- test/unit/spec/MultipleChoice-Spec.js
+++ test/unit/spec/MultipleChoice-Spec.js
@@ -1,12 +1,16 @@
 describe("MultipleChoice", function() {
   
   var deck
+  var topCard
+  var secondCard
   var multipleChoice
 
   beforeEach(function() {
     deck = new Deck()
-    deck.add(new Card("question1", "answer1"))
-    deck.add(new Card("question2", "answer2"))
+    topCard = new Card("question1", "answer1");
+    secondCard = new Card("question2", "answer2")
+    deck.add(topCard)
+    deck.add(secondCard)
     deck.add(new Card("question3", "answer3"))
     deck.add(new Card("question4", "answer4"))
     deck.add(new Card("question5", "answer5"))
@@ -16,7 +20,16 @@ describe("MultipleChoice", function() {
   it("can get a choice of 4 cards containing the current top card", function() {
     var choices = multipleChoice.choices()
 
-    //expect(choices.includes(new Card("question1", "answer1"))).toBe(true)
+    expect(choices.includes(topCard)).toEqual(true)
+    expect(choices.size()).toEqual(4)
+  });
+
+  it("can get a choice of 4 cards after a draw containing the new current top card (second card)", function() {
+    deck.draw()
+
+    var choices = multipleChoice.choices()
+    
+    expect(choices.includes(secondCard)).toEqual(true)
     expect(choices.size()).toEqual(4)
   });
 })
