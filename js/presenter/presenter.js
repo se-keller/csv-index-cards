13775425deca156e3c64656cfde44ab59099f40f
se-keller <sebastian.sekell@googlemail.com>
2016-04-10 20:38:09 +0200

    using 3 dots and shifting long urls from left to right. If the text is too big you can scroll the text if needed

--- js/presenter/presenter.js
+++ js/presenter/presenter.js
@@ -5,8 +5,6 @@ $(document).ready(function() {
 	csvUrlRepository = new CsvUrlLocalStorage()
 	refreshUsedCsvUrls()
     $("#button-load-csv").click(function(){loadCsv($('#input-csv-url').val())})
-    
-
 });
 
 
@@ -35,7 +33,7 @@ function refreshUsedCsvUrls() {
         $("#input-csv-url").val(csvUrls[0])
         $("#csv-urls").empty()
         for(var i=1; i<csvUrls.length; i++) {
-            $("#csv-urls").append('<li><a id="csv-url-'+ i + '" style="white-space: normal !important; word-wrap:break-word; overflow-wrap: break-word;" data-transition="slide" href="#card-front" type="button" data-role="button">' + csvUrls[i] +'</a></li>')
+            $("#csv-urls").append('<li><a id="csv-url-'+ i + '" style="direction:rtl;overflow:auto;" class="ui-btn ui-shadow ui-corner-all" data-transition="slide" href="#card-front" type="button">' + csvUrls[i] +'</a></li>')
             $('#csv-url-'+i).click(function(){
                 $('#input-csv-url').val($(this).text())
                 loadCsv($(this).text())
