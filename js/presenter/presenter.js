ce500073269fae86c26135a0f5cfece0c07c5d46
se-keller <sebastian.sekell@googlemail.com>
2016-04-10 13:51:27 +0200

    remeber the last entered csv url

--- js/presenter/presenter.js
+++ js/presenter/presenter.js
@@ -1,22 +1,22 @@
 var deck
 var csvUrlRepository
 
-$(document).ready(function() {
+$(document).ready(function() {    
 	csvUrlRepository = new CsvUrlLocalStorage()
-	if(!csvUrlRepository.isEmpty())
-		$("#input-csv-url").val(csvUrlRepository.getAll()[0])
-    $("#button-load-csv").click(loadCsv)
+	refreshUsedCsvUrls()
+    $("#button-load-csv").click(function(){loadCsv($('#input-csv-url').val())})
     
 
 });
 
 
-function loadCsv() {
-    new DeckUnmarshaller().fromCsvUrl($('#input-csv-url').val(), dataReceived, errorLoadingCsv)
+function loadCsv(csvUrl) {
+    new DeckUnmarshaller().fromCsvUrl(csvUrl, dataReceived, errorLoadingCsv)
 }
 
 function dataReceived(data) {
 	csvUrlRepository.add($('#input-csv-url').val())
+    refreshUsedCsvUrls()
     deck = data
     showCurrentFront()    
 }
@@ -27,6 +27,22 @@ function progressVal() {
 
 function errorLoadingCsv() {
     $( "#popup-error-loading-csv" ).popup("open")
+}
+
+function refreshUsedCsvUrls() {
+    if(!csvUrlRepository.isEmpty()) {
+        var csvUrls = csvUrlRepository.getAll()
+        $("#input-csv-url").val(csvUrls[0])
+        $("#csv-urls").empty()
+        for(var i=1; i<csvUrls.length; i++) {
+            $("#csv-urls").append('<li><a id="csv-url-'+ i + '" style="white-space: normal !important; word-wrap:break-word; overflow-wrap: break-word;" data-transition="slide" href="#card-front" type="button" data-role="button">' + csvUrls[i] +'</a></li>')
+            $('#csv-url-'+i).click(function(){
+                $('#input-csv-url').val($(this).text())
+                loadCsv($(this).text())
+            })
+        }
+        $("#csv-urls").listview('refresh')
+    }
 }
 
 function setCardValue(value, valueSelector, imgSelector) {
