3b4ced869fe032c5c6a06ccadfc9ffdfe4ceac21
se-keller <sebastian.sekell@googlemail.com>
2020-02-08 22:21:37 +0100

    reading from file system not longer supported by many browsers. Test no longer helpful.

--- test/unit/spec/DeckUnmarshaller-integration-spec.js
+++ test/unit/spec/DeckUnmarshaller-integration-spec.js
@@ -4,26 +4,6 @@ describe("DeckUnmarshaller (Integration)", function() {
   beforeEach(function() {
     unmarshaller = new DeckUnmarshaller()
   });
-describe("when reading from a readable csv-url", function() { 
-    var deck
-    beforeEach(function(done) {
-        unmarshaller.fromCsvUrl(
-        "resources/example.csv", 
-        function(data) {
-          deck = data
-          done()
-        }, 
-        function() {
-          fail("Should not raise an error while converting from csv URL")
-          done()
-        }
-      )
-    })
-
-    it("it converts it into a deck", function() {  
-      expect(deck.size()).toEqual(19)
-    });
-  });
 
   describe("when reading from a unreadable csv-url", function() { 
     var hasError = false
